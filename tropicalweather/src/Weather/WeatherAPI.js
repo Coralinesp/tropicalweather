b34465a3fd6929b7746f7fac35ccf72e19b31443
Coralinesp <perezcruzcaroline@gmail.com>
2025-03-11 08:40:15 -0400

    .

--- tropicalweather/src/Weather/WeatherAPI.js
+++ tropicalweather/src/Weather/WeatherAPI.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Función para obtener los datos del clima usando el nombre de la ciudad
 export const getWeatherByCity = async (cityName) => {
-    const apiKey = '009077ab97b20abae545666fadb5493e'; // Tu clave de API de OpenWeather
+    const apiKey = 'your-api-key';
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric&lang=es`; // lang=es, es para obtener la respuesta en español
 
    
@@ -14,11 +14,10 @@ export const getWeatherByCity = async (cityName) => {
         const { lat, lon } = weatherData.coord;
         const uvUrl = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`;
         const uvResponse = await axios.get(uvUrl);
-        const uvIndex = uvResponse.data.value; // Obtén el índice UV
+        const uvIndex = uvResponse.data.value;
 
-        // Añadir la humedad y el índice UV a los datos del clima
-        weatherData.main.humidity = weatherData.main.humidity; // Humedad
-        weatherData.uvIndex = uvIndex; // Índice UV
+        weatherData.main.humidity = weatherData.main.humidity; 
+        weatherData.uvIndex = uvIndex; 
 
         return weatherData;
     } catch (error) {
@@ -32,11 +31,11 @@ export const fetchWeatherForAllProvinces = async (provinces, setWeatherDataMap) 
     for (const province of provinces) {
         try {
             const weather = await getWeatherByCity(province.name);
-            weatherMap[province.name] = weather; // Guardamos el clima de cada provincia
+            weatherMap[province.name] = weather; 
         } catch (error) {
             console.error(`Error al obtener clima de ${province.name}:`, error);
             weatherMap[province.name] = null;
         }
     }
-    setWeatherDataMap(weatherMap); // Actualizamos el estado con los datos del clima
+    setWeatherDataMap(weatherMap); 
 };
